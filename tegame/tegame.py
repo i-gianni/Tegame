03c85644629ff351e0b6990c09bd044037003051
i-gianni <ivangianni@live.it>
2025-09-09 00:34:20 +0200

    Scan hand added to class tegame

--- tegame/tegame.py
+++ tegame/tegame.py
@@ -5,12 +5,14 @@ from copy import deepcopy
 
 class tegame:
 
-    def __init__(self):
+    def __init__(self,verbose=False):
 
         self.n_players = 2
         self.n_draw = 2
         self.thresh_pile = 10
         self.tresh_nonmandatory = 2
+
+        self.verbose = verbose
 
         card_per_player = {2:7, 3:6, 4:6, 5:6}
         self.n_inhand = card_per_player[self.n_players]
@@ -62,20 +64,74 @@ class tegame:
         for hand in self.hands:
             print(" ".join(f"{num:3n}" for num in hand))
 
-
     def run_game(self):
     #Convention: decreasing piles start with 100 but for sake of ease
     # we consider them at -100 so that they are increasing
     # in this way we simply change the sign for cards in hands
     
         N = self.n_players
+        game_over = False
 
-        while True:
-            for player_idx in range(N):
-                self.play_turn(player_idx)
-                if self.hands==[[]*N]: break
+        while game_over != True:
+            for active_player in range(N):
+                game_over, game_status = self.play_turn(active_player)
 
-        print('You Won!')
+        if game_status == 0:
+            print('You Won!')
+        else:
+            print('Game Over')
+    
+    def scan_hand(self,active_player,discards=[],mandatory_move=True):
+
+        delta,ind = self.scan_hand_(active_player,discards=discards,mandatory_move=mandatory_move)
+        if ind==[]:
+            return delta,[None,None]
+        elif len(ind)==2:
+            return delta,ind
+        else:
+            raise ValueError("Output of scan_hand not recognized")
+            
+    # Look for the best card to play from current player's hand onto the field
+    # return the card, the pile and the damage done by the possible move
+    # delta is the distance of the bast match (the so called damage)
+    # ind contains first the in-hand index then the pile index
+    def scan_hand_(self,active_player,discards=[],mandatory_move=True):
+
+        tops = [pile[-1] for pile in self.piles]
+        delta = 100
+        ind = []
+
+        hand = self.hands[active_player]
+
+        for i, hand_i in enumerate(hand):
+            for j, top_j in enumerate(tops):
+
+                if j in discards: continue
+
+                old_delta = delta
+                old_ind = ind
+
+                ind = [i,j]
+                delta = hand_i - top_j
+                if j > 1: delta = -delta #inversion due to descending piles
+
+                if delta == -10:
+                    return delta, ind
+                elif delta >= old_delta or delta < 0 : #can't be right to simply accept negative delta, check later
+                    delta = old_delta
+                    ind = old_ind
+
+        #replace this with exit program command when the code is implemented in a script
+        if delta==100 and discards==[] and mandatory_move:
+            self.game_over = True
+            self.game_status = 1
+
+        if self.verbose:
+            print(f"\nPlayer {active_player} would like to play card {hand[ind[0]]}")
+            print(f"On pile {ind[1]}")
+            print(f"Dealing {delta} damage")
+
+        return delta, ind
 
 
 
@@ -93,7 +149,7 @@ def play_turn(player_idx):
 
     while True:
         hand_old=hand.copy()
-        hand=play_card(hand,player_idx,obligatory_move=False)
+        hand=play_card(hand,player_idx,mandatory_move=False)
         #print(hand,hand_old)
         if hand==hand_old: break
         n_played = n_played + 1
@@ -105,11 +161,11 @@ def play_turn(player_idx):
 
 # Main action in a player turn
 # perform several actions to undestand which card to play
-def play_card(hand,player_idx,obligatory_move=True):
-    delta_1,(card_idx1,pile_idx1)=scan_hand(hand,obligatory_move=obligatory_move)
+def play_card(hand,player_idx,mandatory_move=True):
+    delta_1,(card_idx1,pile_idx1)=scan_hand(hand,mandatory_move=mandatory_move)
     if delta_1==-10:
         hand=place_card(hand,card_idx1,pile_idx1)
-    elif not obligatory_move and delta_1>one_more_tresh:
+    elif not mandatory_move and delta_1>one_more_tresh:
         return hand
     else:
         #mask_inactive = [i!=player_idx for i in range(N)]
@@ -121,49 +177,16 @@ def play_card(hand,player_idx,obligatory_move=True):
         elif dont_pile_idx_list==[0,1,2,3]: #case 2: all piles are taken, so fuck it
             hand=place_card(hand,card_idx1,pile_idx1)
         else: #case 3: check other piles
-            delta_2,(card_idx2,pile_idx2)=scan_hand(hand,discards=dont_pile_idx_list,obligatory_move=obligatory_move)
+            delta_2,(card_idx2,pile_idx2)=scan_hand(hand,discards=dont_pile_idx_list,mandatory_move=mandatory_move)
             if delta_2-delta_1>danger_thresh or delta_2==100:
                 hand=place_card(hand,card_idx1,pile_idx1)
             else:
-                if not obligatory_move and delta_2>one_more_tresh:
+                if not mandatory_move and delta_2>one_more_tresh:
                     return hand
                 else: hand=place_card(hand,card_idx2,pile_idx2)
     return hand    
 
 # Wrapper to actual scan_hand, correctly unpacks hand and pile indexes
-def scan_hand(hand,discards=[],obligatory_move=True):
-    delta,ind = scan_hand_(hand,discards=discards,obligatory_move=obligatory_move)
-    if ind==[]:
-        return delta,[None,None]
-    elif len(ind)==2:
-        return delta,ind
-    else:
-        raise ValueError("Output of scan_hand not recognized")
-        
-# Look for the best card to play form players hand onto the field
-# return the card, the pile and the damage done by the possible move
-def scan_hand_(hand,discards=[],obligatory_move=True):
-# delta is the distance of the bast match
-# ind contains first the in hand index then the pile index
-    tops = [pile[-1] for pile in piles]
-    delta = 100
-    ind = []
-    for i, hand_i in enumerate(hand):
-        for j, top_j in enumerate(tops):
-            if j in discards: continue
-            old_delta = delta
-            old_ind = ind
-            delta = hand_i - top_j
-            if j > 1: delta = -delta
-            ind = [i,j]
-            if delta == -10:
-                return delta, ind
-            elif delta >= old_delta or delta < 0 :
-                delta = old_delta
-                ind = old_ind
-    #print("Current status:", delta, discards, obligatory_move)
-    if delta==100 and discards==[] and obligatory_move: end_game_and_recap() #replace this with exit program command when the code is implemented in a script
-    return delta, ind
 
 # Modellize the interaction with other players
 # the hand that are passed are of the inactive players
@@ -177,7 +200,7 @@ def interaction(hands):
     delta_list = []
     pile_idx_list = []
     for i in range(n_hands):
-        delta,(card_idx,pile_idx)=scan_hand(hands[i],obligatory_move=False)
+        delta,(card_idx,pile_idx)=scan_hand(hands[i],mandatory_move=False)
         pile_idx_list.append(pile_idx)
         if card_idx is not None: print("Card ",hands[i][card_idx]," playable on pile ",pile_idx,"with delta ",delta)
     print("")
@@ -216,12 +239,6 @@ def print_piles(piles):
         print(string)
     print("")
 
-#End current run due to game over
-def end_game_and_recap():
-    #print_piles(piles)
-    raise ValueError("Game Over")
-
-#Prepare a new game
 
 
 
